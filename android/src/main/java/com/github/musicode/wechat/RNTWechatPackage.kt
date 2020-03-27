e82792010e2810d09a82d933595a3c18a7a8b296
musicode <musicode@qq.com>
2020-03-27 15:16:32 +0800

    bump 0.1.0

--- android/src/main/java/com/github/musicode/wechat/RNTWechatPackage.kt
+++ android/src/main/java/com/github/musicode/wechat/RNTWechatPackage.kt
@@ -1,7 +1,5 @@
 package com.github.musicode.wechat
 
-import java.util.Arrays
-
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
@@ -10,7 +8,7 @@ import com.facebook.react.uimanager.ViewManager
 class RNTWechatPackage : ReactPackage {
 
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-        return Arrays.asList<NativeModule>(RNTWechatModule(reactContext))
+        return listOf<NativeModule>(RNTWechatModule(reactContext))
     }
 
     override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
